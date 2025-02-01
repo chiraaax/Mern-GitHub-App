8a2c83616cd37a1b2710c4dc3dcf3ef2ffc373c7
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2025-02-02 02:41:10 +0530

    ExplorePage-Completed

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -28,38 +28,56 @@ const ExplorePage = () => {
 	};
 	return (
 		<div className='px-4'>
-			<div className='bg-glass max-w-2xl mx-auto rounded-md p-4'>
+			<div className='bg-glass max-w-6xl mx-auto rounded-md p-6'>
 				<h1 className='text-xl font-bold text-center'>Explore Popular Repositories</h1>
-				<div className='flex flex-wrap gap-2 my-2 justify-center'>
+				<div className='flex flex-wrap gap-3 my-4 justify-center'>
 					<img
 						src='/javascript.svg'
 						alt='JavaScript logo'
-						className='h-11 sm:h-20 cursor-pointer'
+						className='h-14 sm:h-24 cursor-pointer'
 						onClick={() => exploreRepos("javascript")}
 					/>					
 					<img
 						src='/php.svg'
 						alt='PHP logo'
-						className='h-11 sm:h-20 cursor-pointer'
+						className='h-14 sm:h-24 cursor-pointer'
 						onClick={() => exploreRepos("php")}
 					/>
 					<img
 						src='/csharp.svg'
 						alt='C# logo'
-						className='h-11 sm:h-20 cursor-pointer'
+						className='h-14 sm:h-24 cursor-pointer'
 						onClick={() => exploreRepos("c#")}
 					/>
 					<img
 						src='/python.svg'
 						alt='Python logo'
-						className='h-11 sm:h-20 cursor-pointer'
+						className='h-14 sm:h-24 cursor-pointer'
 						onClick={() => exploreRepos("python")}
 					/>
 					<img
 						src='/java.svg'
 						alt='Java logo'
-						className='h-11 sm:h-20 cursor-pointer'
+						className='h-14 sm:h-24 cursor-pointer'
 						onClick={() => exploreRepos("java")}
+					/>
+					<img
+						src='/typescript.svg'
+						alt='TypeScript logo'
+						className='h-14 sm:h-24 cursor-pointer'
+						onClick={() => exploreRepos("typescript")}
+					/>
+					<img
+						src='/c++.svg'
+						alt='C++ logo'
+						className='h-14 sm:h-24 cursor-pointer'
+						onClick={() => exploreRepos("c++")}
+					/>
+					<img
+						src='/html.svg'
+						alt='HTML logo'
+						className='h-14 sm:h-24 cursor-pointer'
+						onClick={() => exploreRepos("html")}
 					/>
 				</div>
 				{repos.length > 0 && (
